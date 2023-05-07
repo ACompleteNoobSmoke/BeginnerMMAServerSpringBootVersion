3714b2118b0ae9e39ae9f4dfba23da56484b56d8
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-05-07 16:56:51 -0500

    First Commit

---
+++ src/main/java/com/mmaserver/MMAServer/MmaServerApplication.java
@@ -0,0 +1,13 @@
+package com.mmaserver.MMAServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmaserver/MMAServer/MmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmaserver.MMAServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
